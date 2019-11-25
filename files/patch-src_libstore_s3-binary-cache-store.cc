d76a720787fd691a9082b1620ebd284934d20738
Mateusz Piotrowski <0mp@FreeBSD.org>
2019-11-25 20:26:06 +0100

    Fix further compilation errors related to AWS

--- files/patch-src_libstore_s3-binary-cache-store.cc
+++ files/patch-src_libstore_s3-binary-cache-store.cc
@@ -1,5 +1,14 @@
 --- src/libstore/s3-binary-cache-store.cc.orig	2019-10-10 13:03:46 UTC
 +++ src/libstore/s3-binary-cache-store.cc
+@@ -44,7 +44,7 @@ R && checkAws(const FormatOrString & fs, Aws::Utils::O
+     if (!outcome.IsSuccess())
+         throw S3Error(
+             outcome.GetError().GetErrorType(),
+-            fs.s + ": " + outcome.GetError().GetMessage());
++            fs.s + ": " + outcome.GetError().GetMessage().c_str());
+     return outcome.GetResultWithOwnership();
+ }
+ 
 @@ -54,7 +54,7 @@ class AwsLogger : public Aws::Utils::Logging::Formatte
  
      void ProcessFormattedStatement(Aws::String && statement) override
@@ -45,11 +54,11 @@
              std::shared_ptr<TransferHandle> transferHandle =
                  transferManager->UploadFile(
 -                    stream, bucketName, path, mimeType,
-+                    stream, bucketName.c_str(), path, mimeType,
++                    stream, bucketName.c_str(), path.c_str(), mimeType.c_str(),
                      Aws::Map<Aws::String, Aws::String>(),
                      nullptr /*, contentEncoding */);
  
-@@ -320,8 +320,8 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheSt
+@@ -320,13 +320,13 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheSt
  
              auto request =
                  Aws::S3::Model::PutObjectRequest()
@@ -58,14 +67,35 @@
 +                .WithBucket(bucketName.c_str())
 +                .WithKey(path.c_str());
  
-             request.SetContentType(mimeType);
+-            request.SetContentType(mimeType);
++            request.SetContentType(mimeType.c_str());
  
-@@ -393,7 +393,7 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheSt
+             if (contentEncoding != "")
+-                request.SetContentEncoding(contentEncoding);
++                request.SetContentEncoding(contentEncoding.c_str());
+ 
+             auto stream = std::make_shared<istringstream_nocopy>(data);
+ 
+@@ -393,9 +393,9 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheSt
              auto res = checkAws(format("AWS error listing bucket '%s'") % bucketName,
                  s3Helper.client->ListObjects(
                      Aws::S3::Model::ListObjectsRequest()
 -                    .WithBucket(bucketName)
 +                    .WithBucket(bucketName.c_str())
                      .WithDelimiter("/")
-                     .WithMarker(marker)));
+-                    .WithMarker(marker)));
++                    .WithMarker(marker.c_str())));
  
+             auto & contents = res.GetContents();
+ 
+@@ -404,8 +404,8 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheSt
+ 
+             for (auto object : contents) {
+                 auto & key = object.GetKey();
+-                if (key.size() != 40 || !hasSuffix(key, ".narinfo")) continue;
+-                paths.insert(storeDir + "/" + key.substr(0, key.size() - 8));
++                if (key.size() != 40 || !hasSuffix(key.c_str(), ".narinfo")) continue;
++                paths.insert(storeDir + "/" + key.substr(0, key.size() - 8).c_str());
+             }
+ 
+             marker = res.GetNextMarker();

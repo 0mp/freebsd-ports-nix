ac4f9ee63bb98326960ff66f70782c3b22eb6543
Mateusz Piotrowski <0mp@FreeBSD.org>
2019-11-25 20:03:42 +0100

    Fix some further compilation errors related to AWS

--- files/patch-src_libstore_s3-binary-cache-store.cc
+++ files/patch-src_libstore_s3-binary-cache-store.cc
@@ -1,4 +1,4 @@
---- src/libstore/s3-binary-cache-store.cc.orig	2019-11-25 18:48:38 UTC
+--- src/libstore/s3-binary-cache-store.cc.orig	2019-10-10 13:03:46 UTC
 +++ src/libstore/s3-binary-cache-store.cc
 @@ -54,7 +54,7 @@ class AwsLogger : public Aws::Utils::Logging::Formatte
  
@@ -8,4 +8,55 @@
 +        debug("AWS: %s", chomp((const std::string &)statement));
      }
  };
+ 
+@@ -139,8 +139,8 @@ S3Helper::DownloadResult S3Helper::getObject(
+ 
+     auto request =
+         Aws::S3::Model::GetObjectRequest()
+-        .WithBucket(bucketName)
+-        .WithKey(key);
++        .WithBucket(bucketName.c_str())
++        .WithKey(key.c_str());
+ 
+     request.SetResponseStreamFactory([&]() {
+         return Aws::New<std::stringstream>("STRINGSTREAM");
+@@ -238,8 +238,8 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheSt
+ 
+         auto res = s3Helper.client->HeadObject(
+             Aws::S3::Model::HeadObjectRequest()
+-            .WithBucket(bucketName)
+-            .WithKey(path));
++            .WithBucket(bucketName.c_str())
++            .WithKey(path.c_str()));
+ 
+         if (!res.IsSuccess()) {
+             auto & error = res.GetError();
+@@ -302,7 +302,7 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheSt
+ 
+             std::shared_ptr<TransferHandle> transferHandle =
+                 transferManager->UploadFile(
+-                    stream, bucketName, path, mimeType,
++                    stream, bucketName.c_str(), path, mimeType,
+                     Aws::Map<Aws::String, Aws::String>(),
+                     nullptr /*, contentEncoding */);
+ 
+@@ -320,8 +320,8 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheSt
+ 
+             auto request =
+                 Aws::S3::Model::PutObjectRequest()
+-                .WithBucket(bucketName)
+-                .WithKey(path);
++                .WithBucket(bucketName.c_str())
++                .WithKey(path.c_str());
+ 
+             request.SetContentType(mimeType);
+ 
+@@ -393,7 +393,7 @@ struct S3BinaryCacheStoreImpl : public S3BinaryCacheSt
+             auto res = checkAws(format("AWS error listing bucket '%s'") % bucketName,
+                 s3Helper.client->ListObjects(
+                     Aws::S3::Model::ListObjectsRequest()
+-                    .WithBucket(bucketName)
++                    .WithBucket(bucketName.c_str())
+                     .WithDelimiter("/")
+                     .WithMarker(marker)));
  
